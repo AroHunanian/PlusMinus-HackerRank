929f39e99a07c9062d209e4fe5e9dbfc38697ca1
AroHunanian <88544774+AroHunanian@users.noreply.github.com>
2022-09-25 12:48:48 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -43,8 +43,7 @@ class Solution
         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
         List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
-
-
+        
         Result.plusMinus(arr);
     }
 }
